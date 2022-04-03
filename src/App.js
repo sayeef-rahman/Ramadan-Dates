053983af0b9ac6fdc57a85da2b0f6d85ab70c740
sayeef-rahman <sayeef.cse@gmail.com>
2022-04-03 23:31:28 +0600

    home component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Startd Router</h1>
+      <Routes>
+        <Route></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Reviews/Reviews.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Reviews = () => {
+    return (
+        <div>
+            <h1>in reviews compo</h1>
+        </div>
+    );
+};
+
+export default Reviews;
