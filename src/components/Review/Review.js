2915b26969f338d30f5515be067423172c5083e0
sayeef-rahman <sayeef.cse@gmail.com>
2022-04-04 21:12:09 +0600

    all reviews shoed with styles

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -6,13 +6,13 @@ import './Review.css'
 const Review = (props) => {
     const { review } = props;
     const { rating, name, details, img } = review;
-    console.log(details);
     return (
         <div className='review-container'>
             <img src={img} alt="" />
-            <p>{name}</p>
+            <p className='customer-name'>{name}</p>
+            <p><strong>Rating: <span className='customer-rating'>{rating}</span> </strong> <FontAwesomeIcon icon={faStar} className='star-icon'></FontAwesomeIcon></p>
             <p>{details}</p>
-        </div>  
+        </div>
     );
 };
 
