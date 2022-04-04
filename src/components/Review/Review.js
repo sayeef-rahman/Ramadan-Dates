ab057c1cce53e09e5c637ae492d70d7d520b2bc9
sayeef-rahman <sayeef.cse@gmail.com>
2022-04-04 20:47:55 +0600

    reviews showed withour rating

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,15 +1,18 @@
+import { faCoffee, faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Review.css'
 
 const Review = (props) => {
     const { review } = props;
-    const {rating, name, detail, img } = review;
+    const { rating, name, details, img } = review;
+    console.log(details);
     return (
         <div className='review-container'>
             <img src={img} alt="" />
             <p>{name}</p>
-            <p>{rating} </p>
-        </div>
+            <p>{details}</p>
+        </div>  
     );
 };
 
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import useReviews from '../../hooks/getReviews';
 import Review from '../Review/Review';
+import './Reviews.css'
 
 const Reviews = () => {
     const [reviews, setReviews] = useReviews();
     // console.log(reviews);
     return (
         <div>
-            <div>
+            <h2>See What people say about us.</h2>
+            <div className='reviews-container'>
                 {
                     reviews.map(review =>
                         // console.log(review);
