b587f92f1bb4615f97749b99ed1dffe1acf3b9da
sayeef-rahman <sayeef.cse@gmail.com>
2022-04-04 20:01:56 +0600

    font awesome added

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -2,15 +2,13 @@ import React from 'react';
 import './Review.css'
 
 const Review = (props) => {
-    const {review} = props;
-    const {id,rating,name,detail,img} = review;
+    const { review } = props;
+    const {rating, name, detail, img } = review;
     return (
         <div className='review-container'>
-            <div className='customer-image'>
-                {/* <img src={img} alt="" /> */}
-            </div>
-            {/* <p>{name}</p> */}
-
+            <img src={img} alt="" />
+            <p>{name}</p>
+            <p>{rating} </p>
         </div>
     );
 };
